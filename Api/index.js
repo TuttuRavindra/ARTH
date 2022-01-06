52c6403241da79516e56bc68b41748a81c6b2eda
Enakonda <97044520+TuttuRavindra@users.noreply.github.com>
2022-01-07 00:24:08 +0530

    Adding WebSocket

--- Api/index.js
+++ Api/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const BlockChain = require('../Chain/blockchain');
+const P2pServer = require('./p2p-server');
+const P2pServer = require('./p2p-server');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 
 const bc = new BlockChain();
+
+const P2pServer = new P2pServer(bc);
 
 app.use(bodyParser.json());
 
@@ -24,3 +28,4 @@ app.post('/mine',(req,res)=>{
 });
 
 app.listen(HTTP_PORT,()=>console.log(`Listening on port ${HTTP_PORT}`));
+P2pServer.listen();
