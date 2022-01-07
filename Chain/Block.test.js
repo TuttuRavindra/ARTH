dff68e7ef4ccc8d3fc4e9ded4b149c793af7fb0a
Enakonda <97044520+TuttuRavindra@users.noreply.github.com>
2022-01-07 18:47:31 +0530

    Updated the Block class and its tests

--- Chain/Block.test.js
+++ Chain/Block.test.js
@@ -2,20 +2,36 @@ const Block = require('./block');
 
 describe('Block',()=>{
     
-    let data, lastBlock, block;
+        const timestamp = 2000;
+        const lastHash = 'foo-hash';
+        const hash = 'bar-hash';
+        const data = ['blockchain', 'data'];
+        const nonce = 1;
+        const difficulty = 1;
+        const block = new Block({ timestamp, lastHash, hash, data, nonce, difficulty });
 
-    beforeEach(()=>{
-        const data  = 'bar';
-        const lastBlock = Block.genesis();
-        const block = Block.mineBlock(lastBlock,data);
-    });
+        it('TimeStamp test for the block', () => {
+            expect(block.timestamp).toEqual(timestamp);
+          });
 
-    it('sets the `data` to match the input',()=>{
-        expect(block.data).toEqual(data)
-    });
-    
-    it('sets the `lasthash` to match the hash of the last block',()=>{
-        expect(block.lastHash).toEqual(lastBlock.hash);
-    });
+          it('lastHash test for the block', () => {
+            expect(block.lastHash).toEqual(lastHash);
+          });
+
+          it('hash test for the block', () => {
+            expect(block.hash).toEqual(hash);
+          });
+
+          it('data test for the block', () => {
+            expect(block.data).toEqual(data);
+          });
+
+          it('nonce test for the block', () => {
+            expect(block.nonce).toEqual(nonce);
+          });
+
+          it('difficulty test for the block', () => {
+            expect(block.difficulty).toEqual(difficulty);
+          });
     
 });
--- Chain/block.js
+++ Chain/block.js
@@ -4,7 +4,7 @@ const SHA256 = require('crypto-js/sha256');
 class Block{
     
     //Description--->> TIMESTAMP,HASH,LASTHASH,DATA
-    constructor(timeStamp,hash,lashHash,data,nonce, difficulty){
+    constructor({timeStamp,hash,lashHash,data,nonce, difficulty}){
 
         this.timeStamp = timeStamp;
 
@@ -20,46 +20,46 @@ class Block{
 
     }
 
-    static genesis(){
-        return new this('genesis timestamp','genesis hash','genesis last hash','genesis data', 'genesis nonce','genesis difficulty');
-    }
+    // static genesis(){
+    //     return new this('genesis timestamp','genesis hash','genesis last hash','genesis data', 'genesis nonce','genesis difficulty');
+    // }
 
-    static mineBlock(lastBlock,Data){
+    // static mineBlock(lastBlock,Data){
 
-        const timestamp = Date.now();
-        const lastHash = lastBlock.hash;
-        const hash = Block.hash(lastHash,lastHash,Data);
-        return new this(timestamp,hash,lastHash,Data);
+    //     const timestamp = Date.now();
+    //     const lastHash = lastBlock.hash;
+    //     const hash = Block.hash(lastHash,lastHash,Data);
+    //     return new this(timestamp,hash,lastHash,Data);
 
-    }
+    // }
 
-    static hash(timestamp,lasthash,data){
+    // static hash(timestamp,lasthash,data){
 
-        return SHA256(`${timestamp}${lasthash}${data}`).toString();
+    //     return SHA256(`${timestamp}${lasthash}${data}`).toString();
 
-    }
+    // }
 
-    static blockHash(block){
+    // static blockHash(block){
 
-        const {timeStamp,lastHash,data} = block ;
-        return Block.hash(timeStamp,lastHash,data);
+    //     const {timeStamp,lastHash,data} = block ;
+    //     return Block.hash(timeStamp,lastHash,data);
 
-    }
+    // }
 
 
 
-    toString(){
-        return `Block -
+    // toString(){
+    //     return `Block -
 
-            Timestamp :${this.timeStamp}
-            Last Hash :${this.lashHash}
-            Hash      :${this.hash}
-            Data      :${this.data}
-            Difficulty:${this.difficulty}
-            Nonce     :${this.nonce}  
+    //         Timestamp :${this.timeStamp}
+    //         Last Hash :${this.lashHash}
+    //         Hash      :${this.hash}
+    //         Data      :${this.data}
+    //         Difficulty:${this.difficulty}
+    //         Nonce     :${this.nonce}  
             
-            `;
-    }
+    //         `;
+    // }
 
 }
 
