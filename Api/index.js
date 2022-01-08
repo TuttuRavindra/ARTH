3932d7f60f59c8b98218f902f7d71f329dcc66b2
Enakonda <97044520+TuttuRavindra@users.noreply.github.com>
2022-01-08 12:52:26 +0530

    Added APIs

--- Api/index.js
+++ Api/index.js
@@ -1,32 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const BlockChain = require('../Chain/blockchain');
-const P2pServer = require('./p2p-server');
-
-
-const HTTP_PORT = process.env.HTTP_PORT || 3001;
+const BlockChain = require('../chain/blockchain');
 
 const app = express();
 
-const bc = new BlockChain();
-
-const P2pServer = new P2pServer(bc);
+const blockchain = new BlockChain();
 
 app.use(bodyParser.json());
 
-app.get('/blocks',(req,res)=>{
-    res.json(bc.chain);
+app.get('api/blocks',(req,res)=>{
+    res.json(blockchain.chain);
 });
 
-app.post('/mine',(req,res)=>{
-    const block = bc.addBlock();
-    console.log(`New Block was added:${block.toString()}`);
+// app.post('/mine',(req,res)=>{
+//     const block = bc.addBlock();
+//     console.log(`New Block was added:${block.toString()}`);
 
-    P2pServer.syncChain();
+//     P2pServer.syncChain();
     
-    res.redirect('/blocks');
+//     res.redirect('/blocks');
 
-});
-
-app.listen(HTTP_PORT,()=>console.log(`Listening on port ${HTTP_PORT}`));
-P2pServer.listen();
+// });
+const PORT =3000;
+app.listen(PORT,()=>console.log(`Listening on port at localhost:${PORT}`));
+// P2pServer.listen();
